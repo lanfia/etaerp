345e82d2c02a12d47294657a6fad7cdc0e2e5ecf
Lanfia Cisse <lanfia.cisse@etacell.net>
2018-12-27 08:34:40 +0000

    dd

--- etaerp/etacrm/models.py
+++ etaerp/etacrm/models.py
@@ -1,7 +1,11 @@
 from django.db import models
+fr
 
 class Customer(models.Model):
-    pass
+    customer_reference_number = models.CharField('Customer Reference Number', max_length=20)
+    private_or_company = models.BooleanField()
+
+
 
 class Contact(models.Model):
-    pass
+    customer_reference_number = models.Foreignkey()
